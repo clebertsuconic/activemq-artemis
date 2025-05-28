b0d06b04a0bd4ce9a797b6ffd820af7226be7fef
Clebert Suconic <clebertsuconic@apache.org>
2025-05-28 12:31:04 -0400

    backup

--- tests/db-tests/src/test/java/org/apache/activemq/artemis/tests/db/parallelDB/BasicParallelTest.java
+++ tests/db-tests/src/test/java/org/apache/activemq/artemis/tests/db/parallelDB/BasicParallelTest.java
@@ -61,62 +61,7 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @DisabledIf("isNoDatabaseSelected")
 @ExtendWith(ParameterizedTestExtension.class)
-public class BasicParallelTest extends ParameterDBTestBase {
-
-   private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-   Configuration configuration;
-   DatabaseStorageConfiguration storageConfiguration;
-
-   ExecutorService executorService;
-
-   ScheduledExecutorService scheduledExecutorService;
-
-   ExecutorFactory executorFactory;
-
-   CriticalAnalyzer criticalAnalyzer;
-
-   @Parameters(name = "db={0}")
-   public static Collection<Object[]> parameters() {
-      List<Database> dbList = Database.selectedList();
-      dbList.remove(Database.DERBY); // no derby on this test
-
-      return convertParameters(dbList);
-   }
-
-   // Used in @DisabledIf on class, avoids no-params failure with only -PDB-derby-tests
-   public static boolean isNoDatabaseSelected() {
-      return parameters().isEmpty();
-   }
-
-   @BeforeEach
-   @Override
-   public void setUp() throws Exception {
-      super.setUp();
-      assumeTrue(database != Database.DERBY);
-      dropDatabase();
-   }
-
-   @Override
-   protected final String getJDBCClassName() {
-      return database.getDriverClass();
-   }
-
-   @BeforeEach
-   public void setupTest() throws Exception {
-      storageConfiguration = createDefaultDatabaseStorageConfiguration();
-      this.configuration = createDefaultNettyConfig();
-      configuration.setStoreConfiguration(storageConfiguration);
-
-      executorService = Executors.newFixedThreadPool(5);
-      runAfter(executorService::shutdownNow);
-
-      scheduledExecutorService = Executors.newScheduledThreadPool(5);
-      runAfter(scheduledExecutorService::shutdownNow);
-
-      executorFactory = new OrderedExecutorFactory(executorService);
-      criticalAnalyzer = Mockito.mock(CriticalAnalyzer.class);
-   }
+public class BasicParallelTest extends AbstractStatementTest {
 
    @TestTemplate
    public void testStoreMessage() throws Exception {
@@ -170,26 +115,19 @@ public class BasicParallelTest extends ParameterDBTestBase {
                producer.send(session.createTextMessage("test: " + p));
                session.commit();
                nMessages++;
-               checkCounts(nMessages);
+               checkMessageCounts(nMessages);
             }
             try (Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
                MessageProducer producer = session.createProducer(session.createQueue("TEST"));
                producer.send(session.createTextMessage("test: " + p));
                nMessages++;
-               checkCounts(nMessages);
+               checkMessageCounts(nMessages);
             }
          }
       }
-      checkCounts(nMessages);
+      checkMessageCounts(nMessages);
       server.stop();
 
-   }
-
-   private void checkCounts(int messageCount) throws SQLException {
-      try (Connection jdbcconnection = storageConfiguration.getConnectionProvider().getConnection()) {
-         assertEquals(messageCount, selectCount(jdbcconnection, storageConfiguration.getParallelDBMessages()));
-         assertEquals(messageCount, selectCount(jdbcconnection, storageConfiguration.getParallelDBReferences()));
-      }
    }
 
    @TestTemplate
