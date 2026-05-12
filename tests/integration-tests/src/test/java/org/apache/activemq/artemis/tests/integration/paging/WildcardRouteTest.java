02ab26ea7edcd4b6ef6103d1c1e0e0f9389e8e30
Clebert Suconic <clebertsuconic@apache.org>
2026-05-12 15:41:55 -0400

    ARTEMIS-6056 Throw proper exception if wildcard addresses are used in routing / sending

---
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/paging/WildcardRouteTest.java
@@ -0,0 +1,174 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.activemq.artemis.tests.integration.paging;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import java.lang.invoke.MethodHandles;
+
+import org.apache.activemq.artemis.api.core.QueueConfiguration;
+import org.apache.activemq.artemis.api.core.RoutingType;
+import org.apache.activemq.artemis.api.core.SimpleString;
+import org.apache.activemq.artemis.api.core.client.ClientSession;
+import org.apache.activemq.artemis.api.core.client.ClientSessionFactory;
+import org.apache.activemq.artemis.api.core.client.ServerLocator;
+import org.apache.activemq.artemis.core.paging.PagingStore;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.settings.impl.AddressFullMessagePolicy;
+import org.apache.activemq.artemis.core.settings.impl.AddressSettings;
+import org.apache.activemq.artemis.tests.util.ActiveMQTestBase;
+import org.apache.activemq.artemis.tests.util.CFUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class WildcardRouteTest extends ActiveMQTestBase {
+
+   private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+   protected ActiveMQServer server;
+   protected ClientSession session;
+   protected ClientSessionFactory sf;
+   protected ServerLocator locator;
+
+   final String addressToSend = "a.b.c.d.e.f.g";
+   final String[] queueToReceive = new String[]{"a.b.c.d.e.f.g", "a.b.c.d.e.f.*", "a.b.c.d.e.*.*", "a.b.c.d.*.*.*", "a.b.c.*.*.*.*", "a.b.*.*.*.*.*", "a.*.*.*.*.*.*"};
+   final String addressSettingsMatch = "a.#";
+
+   @Override
+   @BeforeEach
+   public void setUp() throws Exception {
+      super.setUp();
+      server = createServer(true, createDefaultNettyConfig());
+      server.start();
+      locator = createInVMNonHALocator();
+      sf = createSessionFactory(locator);
+      session = addClientSession(sf.createSession(false, true, true));
+   }
+
+   public void printSizes() throws Exception {
+
+      SimpleString[] stores = server.getPagingManager().getStoreNames();
+
+      for (SimpleString q : stores) {
+         PagingStore store = server.getPagingManager().getPageStore(q);
+         logger.info("Store {} has {} elements and {}", store.getAddress(), store.getAddressElements(), store.getAddressSize());
+      }
+   }
+
+   @Test
+   public void testInvalidWildcard() throws Exception {
+
+      server.getAddressSettingsRepository().addMatch(addressSettingsMatch, new AddressSettings().setMaxSizeMessages(100_000).setAddressFullMessagePolicy(AddressFullMessagePolicy.FAIL));
+
+      ConnectionFactory factory = CFUtil.createConnectionFactory("CORE", "tcp://localhost:61616");
+
+      session.createAddress(SimpleString.of(addressToSend), RoutingType.MULTICAST, false);
+      for (String q : queueToReceive) {
+         session.createQueue(QueueConfiguration.of(q).setRoutingType(RoutingType.MULTICAST).setAddress(q));
+      }
+
+      assertThrows(Exception.class, () -> {
+         try (Connection connection = factory.createConnection()) {
+            Session session = connection.createSession(true, Session.SESSION_TRANSACTED);
+            MessageProducer producer = session.createProducer(session.createTopic("a.*.*.*.*.*.*"));
+            producer.send(session.createTextMessage("hello"));
+            session.commit();
+         }
+      });
+   }
+
+   @Test
+   public void testMultipleSends() throws Exception {
+      internalMultipleSends(false);
+   }
+
+   @Test
+   public void testMultipleSendsNoDirectQueue() throws Exception {
+      internalMultipleSends(true);
+   }
+
+   public void internalMultipleSends(boolean skipSendingQueue) throws Exception {
+
+      int numberOfMessages = 10;
+      server.getAddressSettingsRepository().addMatch(addressSettingsMatch, new AddressSettings().setMaxSizeMessages(100_000).setAddressFullMessagePolicy(AddressFullMessagePolicy.FAIL));
+
+      ConnectionFactory factory = CFUtil.createConnectionFactory("CORE", "tcp://localhost:61616");
+
+      session.createAddress(SimpleString.of(addressToSend), RoutingType.MULTICAST, false);
+      for (String q : queueToReceive) {
+         if (skipSendingQueue && q.equals(addressToSend)) {
+            continue;
+         }
+         session.createQueue(QueueConfiguration.of(q).setRoutingType(RoutingType.MULTICAST).setAddress(q));
+      }
+
+      try (Connection connection = factory.createConnection()) {
+         Session session = connection.createSession(true, Session.SESSION_TRANSACTED);
+         MessageProducer producer = session.createProducer(session.createTopic(addressToSend));
+         for (int i = 0; i < numberOfMessages; i++) {
+            producer.send(session.createTextMessage("message " + i));
+         }
+         session.commit();
+      }
+
+      printSizes();
+
+      try (Connection connection = factory.createConnection()) {
+         connection.start();
+         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+         for (String q : queueToReceive) {
+            if (skipSendingQueue && q.equals(addressToSend)) {
+               continue;
+            }
+            MessageConsumer consumer = session.createConsumer(session.createQueue(q + "::" + q));
+            for (int i = 0; i < numberOfMessages; i++) {
+
+               TextMessage message = (TextMessage) consumer.receive(1000);
+               assertNotNull(message);
+               logger.debug("Queue {}, message = {}", q, message.getText());
+            }
+
+            logger.info("> > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > After receiving {}", q);
+            printSizes();
+         }
+      }
+
+      printSizes();
+
+      PagingStore store = server.getPagingManager().getPageStore(SimpleString.of(addressToSend));
+      assertEquals(0L, store.getAddressElements());
+      assertEquals(0, store.getAddressSize());
+
+      for (String q : queueToReceive) {
+         store = server.getPagingManager().getPageStore(SimpleString.of(q));
+         assertEquals(0L, store.getAddressElements());
+         assertEquals(0, store.getAddressSize());
+      }
+   }
+}
